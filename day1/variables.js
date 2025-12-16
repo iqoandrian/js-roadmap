a8d1c741448d2b12509393ab491c143bbcc688b4
Iqo <iqoandrian@gmail.com>
2025-12-16 22:53:44 +0700

    Day 1: Add Variables and Data Types example

---
+++ day1/variables.js
@@ -0,0 +1,26 @@
+// Day 1 - JS Fundamental
+// File: variables.js
+// Topic: Variables and Data Types
+
+// 1. Buat 5 variabel tipe data berbeda
+const nama = "iqo";
+let umur = 30;
+const sudahMenikah = true;
+let alamat = null;
+let pekerjaan;
+
+console.log("=== VARIABLES & DATA TYPES ===");
+console.log("1. String:", nama);
+console.log("2. Number:", umur);
+console.log("3. Boolean:", sudahMenikah);
+console.log("4. Null:", alamat);
+console.log("5. Undefined:", pekerjaan);
+console.log("");
+
+// 2. Type Checking
+console.log("=== TYPE CHECKING ===");
+console.log("Type of nama:", typeof nama);
+console.log("Type of umur:", typeof umur);
+console.log("Type of sudahMenikah:", typeof sudahMenikah);
+console.log("Type of alamat:", typeof alamat);
+console.log("Type of pekerjaan:", typeof pekerjaan);
